b8a67e7b3e34d38a623602e2b25991358ea0132c
RobAntcliff <robertantcliff@gmail.com>
2020-02-02 22:38:13 +0000

    Made getting list simpler

--- HackerNewsConsole/HackerNews.cs
+++ HackerNewsConsole/HackerNews.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace HackerNewsProject
 {
@@ -14,25 +15,28 @@ namespace HackerNewsProject
 
             List<int> storyIds = parseListOfStoryIdsFromJson(data, 100);
 
+            //foreach(int id in storyIds){
+            //    HackerNewsAPI.GetHackerNewsStoriesById(id);
+           // }
+
             return JsonToReturn;
         }
 
         static List<int> parseListOfStoryIdsFromJson(string json, int numberOfIdsToParse){
-            JArray idsJsonArray = JArray.Parse(json);           
+            JArray idsJsonArray = JArray.Parse(json);   
+
+            var list = JsonConvert.DeserializeObject<List<String>>(json);
 
             //If there's less item ids than we actually want 
             //to see then we can only show that many items
-            if(idsJsonArray.Count < numberOfIdsToParse){
-                numberOfIdsToParse = idsJsonArray.Count;
+            if(list.Count > numberOfIdsToParse){
+                list.RemoveRange(numberOfIdsToParse, list.Count - numberOfIdsToParse);
             }
 
             List<int> itemIds = new List<int>();
 
-            //counter to break out of for each when we've gotten as many 
-            int counter = numberOfIdsToParse;
-
             //Go through the array of item Ids and add to list 
-            foreach(var item in idsJsonArray)
+            foreach(var item in list)
             {
                 int number;
                 string itemTemp = item.ToString();
@@ -42,12 +46,6 @@ namespace HackerNewsProject
                 } else {
                     //Throw an exception if the parsing goes wrong because JSON is probably wrong then
                     throw new System.ArgumentException("Something went wrong in Parsing the itemIds. Probably Json is malformed", json);
-                }                
-                
-                counter--;
-                //Break out of loop if we've gotten the amount of Ids that we need
-                if(counter <= 0){
-                    break;
                 }
             }
 
