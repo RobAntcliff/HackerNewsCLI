aff1b72a5f3e28c67835d383fb0841ea0f3c43a8
RobAntcliff <robertantcliff@gmail.com>
2020-02-03 03:16:12 +0000

    Fixing console input error

--- HackerNewsConsole/Console.cs
+++ HackerNewsConsole/Console.cs
@@ -7,25 +7,35 @@ namespace HackerNewsProject
     {
         static async Task Main(string[] args)
         {
+            string errorResponse = "Please pass in arguments in the format --posts n. Where n is how many posts to print. A positive integer <= 100.";
+
             int position = Array.IndexOf(args, "--posts");
 
             //check if --posts is in arguments
             if(position > -1)
             {
                 //Check that there's in integer for number of posts
-                if(args[position + 1] != null 
-                && int.TryParse(args[position + 1], out int result)){
-                    //
-                    if(result > 0 && result < 101){
-                        var generator = new HackerNews();
+                if(args.Length > 1) {
+                    if(args[position + 1] != null
+                    && int.TryParse(args[position + 1], out int result)){
+                        //Make sure integer is within bounds
+                        if(result > 0 && result < 101){
+                            var generator = new HackerNews();
 
-                        string jsonToReturn = await generator.GetXNumberOfTopHackerNewsPosts(result);
+                            string jsonToReturn = await generator.GetXNumberOfTopHackerNewsPosts(result);
 
-                        Console.WriteLine(jsonToReturn);
-                    }                    
-                }                
+                            Console.WriteLine(jsonToReturn);
+                        } else {
+                            Console.WriteLine(errorResponse);
+                        }                      
+                    } else {
+                        Console.WriteLine(errorResponse);
+                    }  
+                } else {
+                    Console.WriteLine(errorResponse);
+                }          
             } else {
-                Console.WriteLine("Please pass in arguments in the format --posts n. Where n is how many posts to print. A positive integer <= 100.");
+                Console.WriteLine(errorResponse);
             }           
         }
     }
